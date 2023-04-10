6d504a2f08a1fc303dd3de1f7057815f7e97df8a
Stranix <Phantom2525@gmail.com>
2023-04-10 11:51:22 +0300

    REFACTOR code style

--- api/vk_api.py
+++ api/vk_api.py
@@ -17,7 +17,11 @@ def get_wall_upload_server(group_id: int, token: str) -> str:
     return upload_url
 
 
-def upload_photo_to_server(upload_url: str, file_name: str):
+def upload_photo_to_server(
+        upload_url: str,
+        file_name: str
+) -> tuple[int, str, str]:
+
     with open(file_name, 'rb') as file:
         files = {
             'photo': file
@@ -26,7 +30,7 @@ def upload_photo_to_server(upload_url: str, file_name: str):
         response.raise_for_status()
         vk_api_response = response.json()
 
-        server = vk_api_response['server']
+        server = int(vk_api_response['server'])
         photo = vk_api_response['photo']
         hash_str = vk_api_response['hash']
 
@@ -39,7 +43,8 @@ def save_wall_photo(
         photo: str,
         hash_str: str,
         token: str
-):
+) -> tuple[int, int]:
+
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
     data = {
         'group_id': group_id,
@@ -51,19 +56,25 @@ def save_wall_photo(
     }
     response = requests.post(url, data=data)
     vk_api_response = response.json()
-    media_id = vk_api_response['response'][0]['id']
-    owner_id = vk_api_response['response'][0]['owner_id']
+    media_id = int(vk_api_response['response'][0]['id'])
+    owner_id = int(vk_api_response['response'][0]['owner_id'])
 
     return media_id, owner_id
 
 
-def wall_post(owner_id: int, group_id: int, from_group: int, photo_id: int,
-              message: str, token: str):
+def wall_post(
+        owner_id: int,
+        group_id: int,
+        photo_id: int,
+        message: str,
+        token: str
+):
+
     url = 'https://api.vk.com/method/wall.post'
     attachments = f'photo{owner_id}_{photo_id}'
     data = {
         'owner_id': f'-{group_id}',
-        'from_group': from_group,
+        'from_group': 1,
         'message': message,
         'attachments': attachments,
         'access_token': token,
