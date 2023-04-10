1f65188d287d6ed32415524bb49f8f327397bb87
Stranix <Phantom2525@gmail.com>
2023-04-10 11:52:29 +0300

    REFACTOR clearing main file

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import os
-import random
-
 import services
-from dotenv import load_dotenv
 
-from api.comics_api import get_comics, get_comics_count
-from api.vk_api import get_wall_upload_server, upload_photo_to_server, \
-    save_wall_photo, wall_post
+from dotenv import load_dotenv
 
 
 def main():
@@ -14,25 +9,7 @@ def main():
     vk_token = os.getenv('VK_ACCESS_TOKEN')
     vk_group_id = int(os.getenv('VK_GROUP_ID'))
 
-    comics_random_num = random.randint(1, get_comics_count())
-    comics_message, file_name = get_comics(comics_random_num)
-
-    photo_upload_url = get_wall_upload_server(vk_group_id, vk_token)
-    server, photo, hash_str = upload_photo_to_server(
-        photo_upload_url,
-        file_name
-    )
-
-    media_id, owner_id = save_wall_photo(
-        vk_group_id,
-        server,
-        photo,
-        hash_str,
-        vk_token
-    )
-
-    wall_post(owner_id, vk_group_id, 1, media_id, comics_message, vk_token)
-    services.remove_file(file_name)
+    services.create_comics_post_on_vk_group(vk_group_id, vk_token)
 
 
 if __name__ == '__main__':
